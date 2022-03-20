1dfea23fd9d4eead80fa2e94b1d6830ba883037c
ssskfl <edik.rogachkov@gmail.com>
2022-03-20 23:49:39 +0300

    added lab 4,5 internet tech

---
+++ src/InternetTech/Lab6/scripts/task2.js
@@ -0,0 +1,10 @@
+var x = prompt("Type a number :");
+
+for(i = 1;i<=10;i++){
+    
+    document.write(i*x+" ");
+}
+document.write("<br>")
+document.write("<br>")
+
+
---
+++ src/InternetTech/Lab7/scripts/task1.js
@@ -0,0 +1,6 @@
+function beg(){
+    for(i = 1;i<1000;i++){
+        id1.style.left = 10000 + "px";
+    }
+}
+beg();
